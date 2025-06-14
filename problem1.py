ffb035f432871a71798141dd589f267fcd34d08e
Delegate-is <kelvinjuma903@gmail.com>
2025-06-14 16:16:16 +0300

    GOtta Input

---
+++ problem1.py
@@ -0,0 +1,5 @@
+# Area of a Cicle Calculator
+radius = float(input("Enter the radius of the circle: "))
+circle_area = 3.14 * radius *radius
+print("The area of the circle is " + str(circle_area))
+print(circle_area)
---
+++ problem2.py
@@ -0,0 +1,8 @@
+name = input("Enter user name: ")
+age = int(input("Enter user age: "))
+print("The user name is " + name + " and age is " + str(age))
+print(f"The user name is {name} and age is {age}")
+
+address = input("Enter your address: ")
+marks = float(input("Enter yoour marks: "))
+print(f"Your address is {address} and your marks is {marks}")
