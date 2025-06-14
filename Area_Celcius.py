5d70765f7f25fd1e26952d7a4b607ed11aa40f27
Delegate-is <kelvinjuma903@gmail.com>
2025-06-14 15:10:10 +0300

    More python fun

---
+++ Area_Celcius.py
@@ -0,0 +1,18 @@
+# Getting user input
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+ 
+# Calculating area
+area = length * width
+ 
+# Displaying output
+print("The area of the rectangle is:", area)
+
+# Getting user input
+celsius = float(input("Enter the temperature in Celsius: "))
+ 
+# Converting temperature to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+ 
+# Displaying output
+print("The temperature in Fahrenheit is:", fahrenheit)
