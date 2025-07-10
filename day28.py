6c18912aef45e7f523605c7b6d6be4b7b79cde8d
Delegate-is <kelvinjuma903@gmail.com>
2025-07-10 07:58:11 +0300

    if

---
+++ day28.py
@@ -0,0 +1,9 @@
+# Python Decision Making Structure
+# if statement in python
+a = 10
+b = 20
+if a < b:
+    print("a is less than b")  # This will be printed since 10 is less than 20  
+else:
+    print("a is not less than b")
+print(a is b)
