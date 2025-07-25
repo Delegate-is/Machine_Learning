53f61ec65f248d2291ac7b82f6f500e78b9f738b
Delegate-is <kelvinjuma903@gmail.com>
2025-07-25 12:26:09 +0300

    Day 40

---
+++ Day40.py
@@ -0,0 +1,34 @@
+# Find circle and square area
+r = float(input("Enter a radius value: "))
+pi = 3.14
+area = pi*r*r
+print("area of a circle = "+str(area))
+
+l = float(input("Enter a length value: "))
+w = float(input("Enter a width value: "))
+area_square = l*w
+print("area of square = "+str(area_square))
+
+# get sentence from user and count total
+s = input("Enter a sentence: ")
+total_char = len(s)
+print(total_char)
+if total_char >= 11:
+    print("VALID")
+else:
+    print("INVALID")
+    
+# Percentage calculator
+total_ammount = float(input("Enter a total ammount: "))
+percentage_required = float(input("Enter a how much percentage you need: "))
+per = (total_ammount *percentage_required)/ 100
+print("Total ammount = "+str(total_ammount))
+print("Required percentage = "+str(percentage_required))
+print("Percentage = "+str(per))
+
+# Get user name = 8 / <20 and only alpha/ numeric data without white space or special chars
+user_name = input("Enter username: ")
+if user_name.isalnum() and (len(user_name) == 8 or len(user_name) < 20):
+    print("User name "+str(user_name)+" is valid.")
+else:
+    print("Username is invalid")
