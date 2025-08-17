74af943afc5844a49d99ddac30e7a7bd32f1c0f7
Delegate-is <kelvinjuma903@gmail.com>
2025-08-17 09:35:29 +0300

    Tupples and 2D arrays

--- IntProb35.py
+++ IntProb35.py
@@ -24,3 +24,30 @@ ar = np.array([
 ])
 new_ar = np.zeros_like(ar)
 print(new_ar)
+
+# Get num from user in array and find max
+import numpy as np
+
+numbers = np.array([int(input(f"Enter your number-{i+1}: ")) for i in range(5)])
+print("Array:", numbers)
+print("Maximum:", np.max(numbers))
+
+char = input("Enter a char")# a e i o u or = ||
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print("It is vowel")
+else:
+    print("It is cons..")
+    
+user = input("Enter username ")
+def check_user(u):
+    if u.isalnum():
+        print("Your Username "+str(u)+" is Okay")
+    else:
+        print("Sorry, use only alpha or numaric without whitespace")
+check_user(user)
+
+day = input("Enter name days: ").strip().lower()
+if day == "sunday":
+    print("Holiday")
+else:
+    print("Not holiday")
